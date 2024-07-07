db49a63d7d7f2470235315731577d911e555b245
mohdmushtaq <mohdmushtaq251@gmail.com>
2024-07-07 19:17:38 +0530

    Moved Images array inside the hook

--- src/app/ImageComp.js
+++ src/app/ImageComp.js
@@ -1,31 +1,39 @@
 "use client";
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function ImageComp() {
   const [image, setImage] = useState('/M.jpeg');
-  const images = ['/M.jpeg','/MMH.jpg']
-  const [fade,setFade]=useState(false)
+  const [fade, setFade] = useState(false);
+
   useEffect(() => {
+    const images = ['/M.jpeg', '/MMH.jpg']; 
+
     const intervalId = setInterval(() => {
-      setFade(true); 
+      setFade(true);
 
       setTimeout(() => {
         setImage((prevImage) => {
-          
           const currentIndex = images.indexOf(prevImage);
           const nextIndex = (currentIndex + 1) % images.length;
           return images[nextIndex];
         });
-        setFade(false); 
-      }, 1000); 
-    }, 2000); 
-    
+        setFade(false);
+      }, 1000);
+    }, 2000);
+
     return () => clearInterval(intervalId);
-  }, [images]);
-  
+  }, []);
+
   return (
     <div>
-      <img src={image} alt="M" width={270} height={200} className={`img-transition ${fade ? 'fade-out' : ''}`}/>
+      <Image
+        src={image}
+        alt="M"
+        width={270}
+        height={200}
+        className={`img-transition ${fade ? 'fade-out' : ''}`}
+      />
     </div>
   );
 }
