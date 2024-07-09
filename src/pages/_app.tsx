315c0b29b2746e1bbc62437515751959d403eb4f
mohdmushtaq <mohdmushtaq251@gmail.com>
2024-07-10 03:00:58 +0530

    setActiveSection in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 // src/pages/_app.tsx
+"use client"
+import React,{useState} from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../app/globals.css'; // Ensure the path to your global CSS is correct
 import ScrollTo from "../app/ScrollTo";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [activeSection, setActiveSection] = useState("#home");
   return (
     <>
+
       <Head>
         <title>Mushtaq&apos;s Portfolio</title>
       </Head>
       <Component {...pageProps} />
-      <ScrollTo />
+      <ScrollTo setActiveSection={setActiveSection}/>
     </>
   );
 }
