a0f0784bf7b550639f5f1cd3bf94a5ced8d3125d
Mohd Mushtaq <126546049+IOutis@users.noreply.github.com>
2024-07-15 16:05:24 +0530

    Update Contact.js

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
       </div>
 
       <div>
-        <a href="www.linkedin.com/in/md-mushtaq-hussain" target='_blank'>
+        <a href="https://www.linkedin.com/in/md-mushtaq-hussain" target='_blank'>
           <svg className="h-8 w-8 text-yellow-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
             <rect x="2" y="9" width="4" height="12" />
